3a99323399406f84d79f72005b215e2b62d34349
Nguyen Vo Duy Tu Vinh <tuvinh0106@gmail.com>
2025-01-27 15:37:29 +0700

    add:cross-build

---
+++ example11-cross-build/hello3/hello1.go
@@ -0,0 +1,6 @@
+package hello3
+
+// Hello is hello world
+func Hello() string {
+	return "build on berfore go1.8 version"
+}
---
+++ example11-cross-build/hello3/hello2.go
@@ -0,0 +1,6 @@
+package hello3
+
+// Hello is hello world
+func Hello() string {
+	return "build on go1.8"
+}
